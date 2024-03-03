dee70efb2e5ed1373ebdd3e22ea3c69cb0db44f9
DavAnders <davidanderson7421@gmail.com>
2024-03-02 22:16:11 -0600

    Add Chi routing

--- main.go
+++ main.go
@@ -3,27 +3,34 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	cfg := &apiConfig{}
+	r := chi.NewRouter() // Chi router
+	cfg := &apiConfig{}  // apiconfig
 	const port = "8080"
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("."))
-	wrappedFileServer := cfg.middlewareMetricsInc(fileServer)
-	mux.Handle("/app/", http.StripPrefix("/app/", wrappedFileServer))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+
+	fileServer := http.FileServer(http.Dir(".")) // project root
+	wrappedFileServer := cfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer))
+	// Chi handles routing now
+	r.Handle("/app/*", wrappedFileServer)
+	r.Handle("/app", wrappedFileServer)
+
+	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
-	mux.Handle("/files/", wrappedFileServer)
-	mux.HandleFunc("/metrics/", cfg.metricsHandler)
-	mux.HandleFunc("/reset/", cfg.resetHandler)
 
-	corsMux := middlewareCors(mux)
+	// r.Handle("/files", wrappedFileServer) // <- not sure why files is included
+	r.Get("/metrics", cfg.metricsHandler) // only GET
+	r.HandleFunc("/reset", cfg.resetHandler)
+
+	corsHandler := middlewareCors(r)
 	server := &http.Server{
-		Handler: corsMux,
+		Handler: corsHandler,
 		Addr:    "localhost:" + port,
 	}
 
