90a6d47e3123d11171045fac8fceffeeddf548d4
DavAnders <davidanderson7421@gmail.com>
2024-03-02 20:16:14 -0600

    Add logo and handler

--- main.go
+++ main.go
@@ -9,7 +9,13 @@ func main() {
 	const port = "8080"
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/", fileServer)
+	mux.Handle("/app/", http.StripPrefix("/app/", fileServer))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	corsMux := middlewareCors(mux)
 	server := &http.Server{
 		Handler: corsMux,
