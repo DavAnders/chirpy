34efa6b699ceefe97b0448fa519e81da275fd55d
DavAnders <davidanderson7421@gmail.com>
2024-03-02 19:32:36 -0600

    add handler for root path

--- main.go
+++ main.go
@@ -8,11 +8,14 @@ import (
 func main() {
 	const port = "8080"
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/", fileServer)
 	corsMux := middlewareCors(mux)
 	server := &http.Server{
 		Handler: corsMux,
 		Addr:    "localhost:" + port,
 	}
+
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
 }
